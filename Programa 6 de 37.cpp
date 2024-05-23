7a6b742cb8492ced5304377dc48acaefa19491ca
BrandonEric <brandon.vazquez.cbtis03@gmail.com>
2024-05-23 16:56:39 -0600

    Este programa fue creado durante la clase

---
+++ Programa 6 de 37.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout<< "El tamaaño de variables de coma flotante es: " << sizeof(float)<<endl;
+    cout<< "Bytes";
+    cout<< "El tamaaño de variables de doble precision es :" << sizeof(double)<<endl;
+    cout<< "Bytes";
+}
