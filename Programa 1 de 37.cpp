6a8891d36579f167b28fd3fd43685e186b12c5d8
BrandonEric <brandon.vazquez.cbtis03@gmail.com>
2024-05-23 10:47:14 -0600

    Este es el 1er programa de 37

---
+++ Programa 1 de 37.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int uni,pre,tot;
+	cout<<"Cantidad de unidades"<<endl;
+	cin>>uni;
+	cout<<"Precio unitario"<<endl;
+	cin>>pre;
+	tot=uni*pre;
+	cout<<"Total :"<<tot<<endl;
+	return 0;
+}
