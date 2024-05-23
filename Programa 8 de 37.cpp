0e655fd570b4ba5f1901245fdf34793164e9c616
BrandonEric <brandon.vazquez.cbtis03@gmail.com>
2024-05-23 16:58:43 -0600

    Este programa fue creado durante la clase

---
+++ Programa 8 de 37.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int m= 45, n=75;
+    cout<< m << n <<endl;
+    ++m;
+    --n;
+    cout<< m << n <<endl;
+    ++m;
+    --n;
+    cout<< m << n  <<endl;
+    ++m;
+    --n;
+}
