0d2afe02abcb5b2e0628e692f72e55961522364c
Vazquez X Brandon Eric <168706025+ericvx11@users.noreply.github.com>
2024-06-03 20:02:13 -0600

    Me equivoqué de archivo, pero es el mismo programa ya corregido

--- Programa 12 aaaaaa.cpp
+++ Programa 12 aaaaaa.cpp
@@ -1,18 +1,11 @@
 #include <iostream>
+#include <cstring>  
 using namespace std;
 
 int main() {
-    int matriz[2][2];
-
-    matriz[0][0] = 10;
-    matriz[0][1] = 20;
-    matriz[1][0] = 30;
-    matriz[1][1] = 40;
-
-    cout<< "El dato en la fila 1 y columna 1 es: " << matriz[0][0] << std::endl;
-    cout<< "El dato en la fila 1 y columna 2 es: " << matriz[0][1] << std::endl;
-    cout<< "El dato en la fila 2 y columna 1 es: " << matriz[1][0] << std::endl;
-    cout<< "El dato en la fila 2 y columna 2 es: " << matriz[1][1] << std::endl;
-
+    char nombre[100];
+    cout << "Escriba su nombre: " << endl;
+    cin.getline(nombre, sizeof(nombre)); 
+    cout << "Hola chavo/a " << nombre << endl;
     return 0;
 }
